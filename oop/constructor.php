118986b3e0d6bd6adcba44390967fb6e91051887
diannurch <diannurcahyaf.com>
2022-10-03 15:39:17 +0700

    video 3 constructor

---
+++ oop/constructor.php
@@ -0,0 +1,41 @@
+<?php
+
+// property : data
+
+use Produk as GlobalProduk;
+
+class Produk
+{
+  public $judul,
+    $penulis,
+    $penerbit,
+    $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga  = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+
+  // method : perilaku
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+
+
+$produk1 = new Produk("Naruto", "Masahi Kishimoto", "Shonen Jump", 30000);
+
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk3 = new Produk("Dragol Ball");
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
