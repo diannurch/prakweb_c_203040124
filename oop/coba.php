70bdc2922e97bda7aeec108e45df36ccfdd13f68
diannurch <diannurcahyaf.com>
2022-10-03 14:19:27 +0700

    video 1 class dan object

---
+++ oop/coba.php
@@ -0,0 +1,20 @@
+<?php
+
+// class: template untuk membuat instance dari objek
+// class mendefinsikan objek
+// menyimpan data dan perilaku yang disebut dengan method dan property
+
+class Coba
+{
+}
+
+
+// objek : instance yg didefinisikan oleh class
+// objek dibuat dengan menggunakan keyword new
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
