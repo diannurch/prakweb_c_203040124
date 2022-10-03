a1df28fb21927165267295f7def2740bc920b51a
diannurch <diannurcahyaf.com>
2022-10-03 10:42:11 +0700

    mengubah background dengan css

--- pw/index.php
+++ pw/index.php
@@ -12,17 +12,10 @@ $buku = query("SELECT * FROM buku");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 
   <!-- Import bootsrap.css -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-  <!-- Bootstrap Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-  <script src="https://kit.fontawesome.com/105baf2792.js" crossorigin="anonymous"></script>
-  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
   <!-- Font style -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -32,10 +25,9 @@ $buku = query("SELECT * FROM buku");
 
 </head>
 
-<body style=" background: linear-gradient(#051923, #003554);">
-
+<body>
   <div class="container">
-    <div class="card mt-4" style="border-radius: 10px;  box-shadow: 0 5px 20px rgba(0,0,0,0.5);">
+    <div class="card mt-4" style="border-radius: 10px; box-shadow: 0 5px 15px rgba(0,0,0,0.5);">
       <div class="card-body text-dark">
         <h2 class="display-1" style="font-family: 'Pattaya'; text-align :center;">Daftar Novelku</h2>
         <div class="add mb-3  btn btn-primary bg-gradient rounded-pill" style="width:180px" ;>
